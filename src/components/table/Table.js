8cd6c8d4ce7d06853496be142fc5fd7d16ce218e
Tabriz Movsumlu <tmovsumlu@oboz.com>
2020-05-18 17:43:05 +0300

    start realizing resize of table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -7,7 +7,31 @@ import {
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
+
+    constructor($root) {
+        super($root, {
+            name: 'Formula',
+            listeners: ['input', 'click']
+        })
+    }
+
     toHTML() {
         return createTable()
+    }
+
+    onClick() {
+        console.log('onClick')
+    }
+
+    onMousedown(event) {
+        console.log('onMousemove', event.target)
+    }
+
+    onMousemove() {
+        console.log('onMousemove')
+    }
+
+    onMouseup() {
+        console.log('onMouseUp')
     }
 }
