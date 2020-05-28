30493396790ac374caef22fa2cdf4909c3b1eb6f
Tabriz Movsumlu <tmovsumlu@oboz.com>
2020-05-28 13:02:15 +0300

    start realizing concept of state, actions, reducers / refactoring

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import {Excel} from '@/components/excel/Excel'
-import {Header} from '@/components/header/Header'
-import {Toolbar} from '@/components/toolbar/Toolbar'
-import {Formula} from '@/components/formula/Formula'
-import {Table} from '@/components/table/Table'
+import {
+  Excel
+} from '@/components/excel/Excel'
+import {
+  Header
+} from '@/components/header/Header'
+import {
+  Toolbar
+} from '@/components/toolbar/Toolbar'
+import {
+  Formula
+} from '@/components/formula/Formula'
+import {
+  Table
+} from '@/components/table/Table'
 import './scss/index.scss'
+import {
+  createStore
+} from './core/createStore'
+import {
+  rootReducer
+} from '@/state/rootReducer'
+
+const store = createStore(rootReducer)
 
 const excel = new Excel('#app', {
-  components: [Header, Toolbar, Formula, Table]
+  components: [Header, Toolbar, Formula, Table],
+  store
 })
 
 excel.render()
